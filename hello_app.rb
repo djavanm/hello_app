ef5d6cc0a0bd1d9d33d7b895efd5e265bb35ca82
Djavan Munroe <djavanm24@gmail.com>
2019-11-30 11:37:13 -0500

    Add day names

--- hello_app.rb
+++ hello_app.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 
 get '/' do
-  'Goodbye, world!'
+  DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday", "Saturday"]
+  dayname = DAYNAMES[Time.now.wday]
+  "Hello, world! Happy #{dayname}."
 end
